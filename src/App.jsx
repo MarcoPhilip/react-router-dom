364e2dde4353452ba68977ea26b115fdb6068ab6
marcophilip <marcophilipagapito@gmail.com>
2025-05-23 00:35:30 -0700

    react router exercise completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState } from 'react';
+import PokemonList from './components/PokemonList/PokemonList';
+import NavBar from './components/NavBar/NavBar';
+import { Route, Routes } from 'react-router';
+import PokemonDetails from './components/PokemonDetails/PokemonDetails';
+import PokemonForm from './components/PokemonForm/PokemonForm';
+
+
+const initialState = [
+  { _id: 1, name: 'bulbasaur', weight: 69, height: 7 },
+  { _id: 2, name: 'ivysaur', weight: 130, height: 10 },
+  { _id: 3, name: 'venusaur', weight: 1000, height: 20 },
+  { _id: 4, name: 'charmander', weight: 85, height: 6 },
+  { _id: 5, name: 'charmeleon', weight: 190, height: 11 },
+];
+
+const App = () => {
+  const [pokemon, setPokemon] = useState(initialState);
+
+    // Form Functionality
+    const addPokemon = (newPokemonData) => {
+
+      // Assign the next available ID
+      newPokemonData._id = pokemon.length + 1;
+
+      // Update the data
+      setPokemon([...pokemon, newPokemonData]);
+  };
 
   return (
-    <h1>Hello world!</h1>
-  )
-}
+    <>
+      <NavBar />
+      <h1>Pokemon!</h1>
+      <Routes>
+        <Route path="/" element={<h2>Home Page</h2>} />
 
-export default App
+        <Route path="/pokemon" element={<PokemonList pokemon={pokemon} />} />
+
+        <Route path="/pokemon/new" element={<PokemonForm />} />
+
+        <Route 
+          path="/pokemon/new" 
+          element={<PokemonForm addPokemon={addPokemon} />}
+        />
+
+        <Route
+          path="/pokemon/:pokemonId"
+          element={<PokemonDetails pokemon={pokemon} />}
+        />
+        
+        <Route path="*" element={<h2>Whoops, nothing here!</h2>} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
